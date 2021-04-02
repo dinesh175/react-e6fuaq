28e417ad0548fe74207bb12c98c3e134b4aeaf29
dinesh175 <vijaydinesh768@gmail.com>
2021-04-02 20:15:23 +0530

    4 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from "react";
 import "./style.css";
-
+import Navbar from "./component/navbar"
+import { BrowserRouter, Route, Switch,Link } from 'react-router-dom';
+import Signup from "./component/signup"
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+
+    <Navbar />
+   
+      <div className="Form">
+        <div className="form-floating mb-3">
+          <input
+            type="email"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">Email address</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label for="floatingPassword">Password</label>
+          <br />
+        </div>
+        <button type="button" class="btn btn-primary">
+          login
+        </button>
+      </div>
     </div>
   );
 }
